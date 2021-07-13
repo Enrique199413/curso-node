19a584a8d0fe440fe21a34c1874d733451164177
aaaAAAF <irma.aleman@gft.com>
2021-07-13 17:22:37 -0500

    util test

--- utils/testUtilsAme/utils.test.js
+++ utils/testUtilsAme/utils.test.js
@@ -8,15 +8,35 @@ const utils = require('../utils')
 chai.use(chaiHttp);
 
 describe('Test utils', () => {
-    it('Equal object', (done) => {
-        const data = ({}, 'name', 'lastName', 'surName')
-        chai.expect(utils.objectUtils.existPropertiesOnObject).to.equal(data)
+    it('Continue success', (done) => {
+        const data = { name: "name", lastName: "lasName", surName: "surName" }
+        const { canContinue } = utils.objectUtils.existPropertiesOnObject(data, 'name', 'lastName', 'surName')
+        chai.expect(canContinue).to.be.true;
         done()
     })
 
+    it('Correct length', (done) => {
+        const data = { Name: "name", LastName: "lastName", SurName: "surName" }
+        chai.expect(Object.keys(data)).to.have.lengthOf(3)
+        done()
+    })
 
+    it('Incorrect length', (done) => {
+        const data = { Name: "name", LastName: "lastName" }
+        chai.expect(data.length < data.length).to.be.false
+        done()
+    })
 
+    it('Not empty', (done) => {
+        const data = {}
+        chai.expect(data).to.be.empty;
+        done()
+    })
 
-
+    it('Equal keys', (done) => {
+        const data = { name: "name", lastName: "lastName", surName: "surName" }
+        chai.expect(data).to.include.all.keys('name', 'lastName', 'surName');
+        done()
+    })
 
 })
